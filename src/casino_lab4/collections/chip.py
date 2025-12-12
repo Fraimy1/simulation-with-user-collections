ea9c0947f35b9de23d8b9d31731c3a97ccc6bdbc
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-12-12 04:46:53 +0300

    [update] project setup

--- src/casino_lab4/collections/chip.py
+++ src/casino_lab4/collections/chip.py
@@ -2,6 +2,6 @@ class Chip:
     def __init__(self, value: float) -> None:
         self.value = value
 
-    def __add__(self, chip_2:Chip):
+    def __add__(self, chip_2: "Chip"):
         return self.value + chip_2.value
 
