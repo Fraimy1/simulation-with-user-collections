4b5ba895ce3d209e35caa2cf5d2c216f320e3b45
Fraimy1 <117653591+Fraimy1@users.noreply.github.com>
2025-12-12 15:42:54 +0300

    [refactor] PlayerCollection and Player

--- src/casino_lab4/collections/players.py
+++ src/casino_lab4/collections/players.py
@@ -1,39 +1,65 @@
+from __future__ import annotations
 from typing import Iterator
 from loguru import logger
 from casino_lab4.utils.logging import log_and_raise
-from casino_lab4.core.errors import NotFoundError
+from casino_lab4.core.errors import OutOfRangeError, WrongTypeError, StepZeroError
 from casino_lab4.domain.player import Player
 
 class PlayerCollection:
-    def __init__(self) -> None:
-        self._data: list[Player] = []
+    def __init__(self, data: list[Player] | None = None) -> None:
+        self._data: list[Player] = data.copy() if data is not None else []
     
     def __len__(self) -> int:
         return len(self._data)
 
     def __setitem__(self, i: int, player: Player) -> None:
-        if i not in range(len(self._data)):
-            log_and_raise(NotFoundError(f"Player at index {i} not found"))
+        if not isinstance(i, int):
+            log_and_raise(WrongTypeError("Index must be an integer"))
         
-        self._data[i] = player
+        try:
+            self._data[i] = player
+        except IndexError:
+            log_and_raise(OutOfRangeError(f"Player at index {i} not found"))
+
         logger.info(f"Player number {i} changed: {player}")
 
-    def __getitem__(self, i: int) -> Player:
-        if i not in range(len(self._data)):
-            log_and_raise(NotFoundError(f"Player at index {i} not found"))
+    def __getitem__(self, i: int|slice) -> Player|PlayerCollection:
+        if not isinstance(i, (int,slice)):
+            log_and_raise(WrongTypeError("Index must be an integer or a slice"))
         
-        return self._data[i]
-    
+        if isinstance(i, slice):
+            if i.step is not None and i.step == 0:
+                log_and_raise(StepZeroError("Step must be non-zero"))
+            
+            return PlayerCollection(self._data[i])
+        else:
+            try:
+                return self._data[i]
+            except IndexError:
+                log_and_raise(OutOfRangeError(f"Player at index {i} not found"))
+        
+        raise NotImplementedError("Index must be an integer or a slice")
+
     def __iter__(self) -> Iterator[Player]:
         return iter(self._data)
 
     def __delitem__(self, i: int) -> None:
-        player = self._data.pop(i)
-        if player is None:
-            log_and_raise(NotFoundError(f"Player at index {i} not found"))
+        if not isinstance(i, int):
+            log_and_raise(WrongTypeError("Index must be an integer"))
         
-        logger.info(f"Player number {i} deleted: {player}")
+        try:
+            self._data.pop(i)
+        except IndexError:
+            log_and_raise(OutOfRangeError(f"Player at index {i} not found"))
+        
+        logger.info(f"Player number {i} deleted")
 
     def append(self, player: Player) -> None:
+        if not isinstance(player, Player):
+            log_and_raise(WrongTypeError("Player must be a Player object"))
+        
         self._data.append(player)
         logger.info(f"Player appended: {player}")
+
+    def __repr__(self) -> str:
+        return f"PlayerCollection({self._data})"
--- src/casino_lab4/domain/player.py
+++ src/casino_lab4/domain/player.py
@@ -2,4 +2,6 @@ class Player:
     def __init__(self, name:str, balance:float) -> None:
         self.name: str = name
         self.balance: float = balance
-        
+    
+    def __repr__(self) -> str:
+        return f"Player(name={self.name}, balance={self.balance})"
